e50e9b5c7f31391e3c9397c595b9dddb9ad5b69d
Vinicius Aloncio de Assunção <vinicius.a.assuncao2003@gmail.com>
2026-08-24 16:06:00 -0300

    feat: adicionar módulo de fase e evaporação de precipitação

--- crates/astronomicon-core/src/math/precipitation.rs
+++ crates/astronomicon-core/src/math/precipitation.rs
@@ -1,5 +1,7 @@
 pub mod bergeron_process;
+pub mod phase_and_evaporation;
 pub mod sedimentation_balance;
 
 pub use bergeron_process::*;
+pub use phase_and_evaporation::*;
 pub use sedimentation_balance::*;
