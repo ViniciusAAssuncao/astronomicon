aba0a36bb0b14a4de83572ff7833d2716e6f7993
Vinicius Aloncio de Assunção <vinicius.a.assuncao2003@gmail.com>
2026-08-31 19:26:55 -0300

    feat: adicionar módulo de contexto e função de construção de contexto

--- crates/rocketcon-app/src/lib.rs
+++ crates/rocketcon-app/src/lib.rs
@@ -1,7 +1,9 @@
+pub mod context;
 pub mod environment;
 pub mod error;
 pub mod universe;
 
+pub use context::*;
 pub use environment::*;
 pub use error::{RocketError, RocketResult};
 pub use universe::*;
